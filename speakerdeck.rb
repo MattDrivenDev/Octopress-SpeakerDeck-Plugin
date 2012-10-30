905cdbe4d766b7224a82222500a6b6e5478e93b8
saxonmatt <saxonmatt@gmail.com>
2012-10-30 14:00:24 +0000

    Initial implementation commit

---
+++ speakerdeck.rb
@@ -0,0 +1,16 @@
+﻿module Jekyll
+    class SpeakerDeck < Liquid::Tag
+  
+        def initialize(name, id, tokens)
+            super
+            @id = id
+        end
+
+        def render(context)
+            %(<script async class="speakerdeck-embed" data-id="#{@id}" src="//speakerdeck.com/assets/embed.js"></script>)
+        end
+
+    end
+end
+
+Liquid::Template.register_tag('speakerdeck', Jekyll::SpeakerDeck)
